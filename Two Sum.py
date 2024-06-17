7cb42d1800248def3aba468f95444ea4e85c5ec6
BhavikaRamesh <77563868+BhavikaRamesh@users.noreply.github.com>
2024-06-17 07:23:49 +0530

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -7,3 +7,18 @@ class Solution:
             if (target - nums[i]) in d and d[target - nums[i]] != i:
                 return [i, d[target - nums[i]]]
         return []
+
+
+// Approach 2 from Interview Bit
+class Solution:
+	# @param A : tuple of integers
+	# @param B : integer
+	# @return a list of integers
+	def twoSum(self, A, B):
+        d = {}
+        for i in range(len(A)):
+            if B - A[i] in d:
+                return [d[B - A[i]] + 1, i + 1]
+            if A[i] not in d:
+                d[A[i]] = i
+        return []
