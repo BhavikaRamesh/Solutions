34eb1091aa2642ffbf9c5c97d7743086f714268f
BhavikaRamesh <77563868+BhavikaRamesh@users.noreply.github.com>
2024-06-22 19:55:00 +0530

    Update isBST.py

--- isBST.py
+++ isBST.py
@@ -1,3 +1,4 @@
+//1st approach doing by following in-order traversal
 class Solution:
     
     #Function to check whether a Binary Tree is BST or not.
@@ -17,4 +18,21 @@ class Solution:
             if lst[i] <= lst[i - 1]:
                 return False
         return True
-        
+// 2nd Approach doing recursively     
+class Solution:
+    
+    #Function to check whether a Binary Tree is BST or not.
+    def isBST(self, root):
+        #code here
+        def helper(root, mini, maxi):
+            if root == None: return True
+            elif mini < root.data and maxi > root.data:
+                return helper(root.left, mini, root.data) and helper(root.right, root.data, maxi)
+            return False
+            
+        lst = []
+        if root == None:
+            return True
+        elif helper(root.left, 0, root.data) and helper(root.right, root.data, 1000000):
+            return True
+        return False
