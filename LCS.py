42b262773072cb4632ea645d9041889a56e50e11
BhavikaRamesh <77563868+BhavikaRamesh@users.noreply.github.com>
2024-07-02 08:23:00 +0530

    Create LCS.py

---
+++ LCS.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        A = text1
+        B = text2
+        dp = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+        for i in range(1, len(A) + 1):
+            for j in range(1, len(B) + 1):
+                if A[i - 1] == B[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[len(A)][len(B)]
